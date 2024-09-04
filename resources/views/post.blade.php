d48300e1ba8365f582eda44e58c336aeaf395370
Bart5690 <88905@glr.nl>
2024-09-04 11:38:26 +0200

    les 14

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,15 @@
-<!doctype html>
-<html>
-<head>
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-<article>
-    <h1><?= $post->title; ?></h1>
-    <div><?= $post->body; ?></div>
-</article>
-<a href="/">Go Back</a>
-</body>
-</html>
+@extends('components.layout')
 
+@section ('content')
+    <article>
+        <h1>
+            {{$post->title }}
+        </h1>
+
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
+
+    <a href="/">Go Back</a>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,25 +1,20 @@
-<!doctype html>
+<x-layout>
 
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
+    @foreach ($posts as $post)
+        <article>
+            <article>
 
-<body>
-<?php foreach ($posts as $post) : ?>
-<article>
+                <h1>
+                    <a href="/posts/{{$post->slug }}">
 
-    <h1>
-        <a href="/posts/<?= $post->slug; ?>">
+                        {{ $post->title }}
+                    </a>
+                </h1>
 
-            <?= $post->title; ?>
-    </h1>
+                <div>
+                    {{ $post->excerpt }}
+                </div>
 
-    <div>
-            <?= $post->excerpt; ?>
-    </div>
-
-
-</article>
-<?php endforeach; ?>
-
-
-</body>
+            </article>
+    @endforeach
+</x-layout>
